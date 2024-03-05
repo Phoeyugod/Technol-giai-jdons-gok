cdc19ecc4b9947044a9f2ed93e62480ff65cf425
phoeyugod <hujberenzomeliton@turr.hu>
2024-03-05 10:32:20 +0100

    kategoriak teszt ?

---
+++ Kategóriák/kategoriak.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const h1Element = document.querySelector('h1');
+    h1Element.style.position = 'absolute';
+    h1Element.style.top = '0';
+    h1Element.style.left = '50%';
+    h1Element.style.transform = 'translateX(-50%)';
+});
