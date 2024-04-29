4b122353de48f90249a0cf2d56543c86fbba6ffe
horvathdanielgabor <you@example.com>
2024-04-29 23:52:19 +0200

    Enzo_munkája_amit_én_töltök_fel_Github_okok_miatt

--- Kategóriák/kategoriak.js
+++ Kategóriák/kategoriak.js
@@ -5,3 +5,26 @@ document.addEventListener('DOMContentLoaded', function() {
     h1Element.style.left = '50%';
     h1Element.style.transform = 'translateX(-50%)';
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+// A modális ablak bezárása, ha a felhasználó az ablakon kívül kattint
+window.onclick = function(event) {
+  var modals = document.getElementsByClassName('modal');
+  for (var i = 0; i < modals.length; i++) {
+      if (event.target == modals[i]) {
+          modals[i].style.display = "none";
+      }
+  }
+}
